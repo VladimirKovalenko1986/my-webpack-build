308c67e7a9191f0d8c72bc58ae49cab4a584a469
Vladimir Kovalenko <dante77758@gmail.com>
2023-11-10 01:24:24 +0200

    add filws

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-webpack.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          //   'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['autoprefixer'],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+
+  devServer: {
+    // open: true,
+    port: 7777,
+  },
+};
